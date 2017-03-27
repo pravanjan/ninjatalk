f6c6d440a979197d09be143ccba1543002f8a9d8
pravanjan <pravanjan.niranjan@a-cti.com>
2017-03-27 21:20:28 +0530

    adding commet

--- src/test/java/v0/com/ninja/helper/PaymentProcessorTest.java
+++ src/test/java/v0/com/ninja/helper/PaymentProcessorTest.java
@@ -2,6 +2,7 @@ package v0.com.ninja.helper;
 
 import static org.junit.Assert.assertEquals;
 
+import org.apache.log4j.Logger;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,6 +11,7 @@ import v0.com.ninja.thirdparty.fake.PayPalFake;
 public class PaymentProcessorTest {
 	
 	public static PaymentProcessor paymentProcessor = null;
+	private static final Logger logger = Logger.getLogger(PaymentProcessorTest.class.getPackage().getName());
 	@BeforeClass 
 	public static  void beforeTest(){
 		paymentProcessor = new PaymentProcessor(new PayPalFake());
@@ -17,6 +19,8 @@ public class PaymentProcessorTest {
 	
 	@Test
 	public void makePaymentTest() {
+		logger.info("The payment processor resposne "+paymentProcessor.processPayment(30));
+		
 		assertEquals(true, paymentProcessor.processPayment(50));
 		
 	}
