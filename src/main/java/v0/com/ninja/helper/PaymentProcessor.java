c2ccab688cbbafd29c4fca4759bb585721adb71b
pravanjan <pravanjan.niranjan@a-cti.com>
2017-03-28 11:25:12 +0530

    initial commit for demo

--- src/main/java/v0/com/ninja/helper/PaymentProcessor.java
+++ src/main/java/v0/com/ninja/helper/PaymentProcessor.java
@@ -3,16 +3,6 @@ package v0.com.ninja.helper;
 import v0.com.ninja.thirdparty.PaymentType;
 
 public class PaymentProcessor {
-	PaymentType paymentType = null;
-	
-	public PaymentProcessor(PaymentType  paymentType ) {
-		this.paymentType = paymentType;
-		
-		
-	}
-	
-   public Boolean  processPayment (int amount){
-	  return  paymentType.makePayment(amount);
-   }
+
 
 }
