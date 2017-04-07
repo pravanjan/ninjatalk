7d1651b3bf4720bf04a7d12ccacfc8a4f3f3f330
pravanjan <pravanjan.niranjan@a-cti.com>
2017-04-07 18:21:58 +0530

    test commit

--- src/test/java/v0/com/ninja/helper/PaymentProcessorTest.java
+++ src/test/java/v0/com/ninja/helper/PaymentProcessorTest.java
@@ -17,44 +17,6 @@ import v0.com.ninja.thirdparty.PayPal;
 @PrepareForTest({PaymentHelper.class})
 public class PaymentProcessorTest {
 	
-	public static PaymentProcessor paymentProcessor   = null ;
-	public static PayPal fakePaypal  = null;
-	
-	
-	
-	@BeforeClass
-	public static void setup (){
-		fakePaypal = Mockito.mock(PayPal.class);
-		when(fakePaypal.makePayment(30)).thenReturn(true);
-		paymentProcessor = new PaymentProcessor(fakePaypal);
-		
-		
-	}
-	
-	@Test 
-	public void testPayPal (){
-		
-		
-		assertEquals(true, paymentProcessor.makePayment(30));
-	}
-	
-	
-	@Test
-	public void testPowerMock(){
-		PowerMockito.mockStatic(PaymentHelper.class);
-		PowerMockito.when(PaymentHelper.processGet("id")).thenReturn("Bye")
-;		assertEquals("Hi", PaymentProcessor.getPayment("id"));
-		
-	}
-	
-	@Test
-	public void testPrivate () throws Exception{
-		
-		PaymentHelper privatemethod = PowerMockito.spy(new PaymentHelper());
-		PowerMockito.doReturn("I am private").when(privatemethod,"privateMethod","soem");
-		assertEquals("I am private",PaymentProcessor.callPrivate("soem"));
-		
-	}
 	
 	
 	
